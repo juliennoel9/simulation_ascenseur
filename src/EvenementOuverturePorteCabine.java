6b2afb945acc504254b35ce7db0a5f8c181ca737
Julien NOEL <julien.noel5@etu.univ-lorraine.fr>
2020-01-24 11:52:36 +0100

    Init commit

---
+++ src/EvenementOuverturePorteCabine.java
@@ -0,0 +1,23 @@
+public class EvenementOuverturePorteCabine extends Evenement {
+    /* OPC: Ouverture Porte Cabine
+       L'instant précis ou la porte termine de s'ouvrir.
+    */
+
+    public EvenementOuverturePorteCabine(long d) {
+	super(d);
+    }
+
+    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+	buffer.append("OPC");
+    }
+
+    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+	Cabine cabine = immeuble.cabine;
+	Etage étage = cabine.étage;
+
+	notYetImplemented();
+	
+	assert cabine.porteOuverte;
+    }
+
+}
